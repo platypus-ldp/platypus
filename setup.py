6066c372781782842ed09213258910166bb7c726
Benjamin Armintor <armintor@gmail.com>
2016-04-09 15:13:48 -0400

    start reorganizing into a python project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='platypus',
+  version='0.1',
+  description='A Python LDP client',
+  url='http://github.com/platypus-ldp/platypus',
+  author='rlskoeser, barmintor, cmh2166, jrhoads',
+  author_email='platypus-ldp@googlegroups.com',
+  license='MIT',
+  packages=['platypus'],
+  zip_safe=False)
